4d6f16ab1eab12312e6698a578d728e325db0cde
vanshu 123 <vanshigoel74@gmail.com>
2017-05-06 08:46:55 +0530

    sf

--- script.js
+++ script.js
@@ -21,8 +21,9 @@ data.forEach(function(product,index){
 	
 	//after clicking order now button
 	$("#order").click(function(){
-		
+	window.location.href="page 2.html";
 	
+	});
 	$.ajax({
 		url:"http://acadprojects.com/py/fabsos/cuisines?location_id=1",
 		type:'GET',
@@ -34,15 +35,24 @@ data.forEach(function(product,index){
 			console.log(response);
 			var data=response["cuisines"]
 			data.forEach(function(product,index){
+				var elem=$("<div class='cont col-md-4'></div>");
+				var img=$("<img id='img1'>").attr("src",product["image"]);
 				var names=$("<div class='name'></div>").text(product["cuisine_name"]);
-			console.log(names)
+		
+			//console.log(img);
+			elem=$(elem).append(img,names);
+			$(".cuisine").append(elem);
+			//var name=$($(this).find("names")).text();
+			//var imgf=$($(this).find("img")).attr();
+			//localStorage.setItem("cuisinename",name);
+			//localStorage.setItem("images",imgf);
+					
 			
 		});
 		}
 		
 		
-	});
-	window.location.href="page 2.html";
+	
 	});
 	
 		
