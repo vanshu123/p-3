7b77652682b863d9c0b85882ede50dd29d7abe82
vanshi12 <vanshikagoel1@gmail.com>
2017-06-27 22:41:29 +0530

    new3

--- script.js
+++ script.js
@@ -47,36 +47,69 @@ $("#dropdown").change(function(){
 				var names=$("<div class='name'></div>").text(product["cuisine_name"]);
 				var id=$("<div class='proid'></div>").text(product["id"]);
 
+				
 			//console.log(img);
 			elem=$(elem).append(img,names,id);
 			$(".cuisine").append(elem);
 
 			});
+			
+			
+	
 			$(".cont").click(function(){
 				var i=$($(this).find(".proid")).text();
 				localStorage.setItem("id",i);
 				window.location.href="page 3.html";
+			});
+			$(".drop").change(function() {
+				var a=$($(this).find(".proid")).text();
+				localStorage.setItem("ida",a);
+				window.location.href="page 3.html";
+				localStorage.setItem("cuisine",this.value);
 			});
 	}
 
 		});
 		var idvalue=parseInt(localStorage.getItem("id"));
 
-
-	$.ajax ({
+$.ajax({
+		url:"http://acadprojects.com/py/fabsos/cuisines?location_id=1",
+		type:'GET',
+		data:{
+"location_id":"1"
+		},
+		success:function(response) {
+			console.log(response);
+			var data= response["cuisines"];
+			data.forEach(function(product,index){
+				var cname=$("<option class='copt'></option>").text(product["cuisine_name"]);
+				var cid=$("<option class='aaid'></option>").text(product["id"]);
+				$(".drop").append(cname);
+			});
+			
+		}
+});
+$(".drop").change(function() {
+	//var cuopt=localStorage.getItem("id");
+	var cuopt=parseInt(localStorage.getItem("ida"));
+		$.ajax ({
 		url:"https://acadprojects.com/py/fabsos/items?cuisines_id=153",
 		type:'GET',
 	    data: {
-"cuisines_id":idvalue
+"cuisines_id":cuopt
 		},
 		success:function(response){
-			console.log(response);
+			console.log(response);			
 			var data=response["data"];
-			data.forEach(function(product,index){
+			data.forEach(function(product,index){	
+			
 			var elemen="<div class='fooditems col-md-4 col-md-offset-2'></div>";
 			var imag=$("<img id='ima'>").attr("src",product["image"]);
 			var price=$("<div class='foodp'></div>").text(product["price"]);
 			var foodname=$("<div class='foodn'></div>").text(product["item_name"]);
+			var sub="<input id='subt1' name='subtract' value='-' type='button'>";
+			var box="<input id='qty' name='qty' value='0' style='width: 50px; text-align: center;' type='text'>";
+			var add="<input id='add1' name='add' value='+' type='button'>"
 			var veg=$("<span class='vegetarianicon'>⊡</span>");
 			var nv="<img src='img/nv.jpg' class='nonv'>";
 			//var non=$("<div class='abcd'></div>").text(product["is_vegetarian"]);
@@ -87,8 +120,51 @@ $("#dropdown").change(function(){
 			else {
 			foodname=$(foodname).append(nv);
 			}
-			
+			foodname=$(foodname).append(sub,box,add);
 			elemen=$(elemen).append(imag,price,foodname);
+			
+			$(".area").append(elemen);
+			
+			});
+		}
+
+	});
+});
+
+
+				
+			
+	$.ajax ({
+		url:"https://acadprojects.com/py/fabsos/items?cuisines_id=153",
+		type:'GET',
+	    data: {
+"cuisines_id":idvalue
+		},
+		success:function(response){
+			console.log(response);			
+			var data=response["data"];
+			data.forEach(function(product,index){	
+			
+			var elemen="<div class='fooditems col-md-4 col-md-offset-2'></div>";
+			var imag=$("<img id='ima'>").attr("src",product["image"]);
+			var price=$("<div class='foodp'></div>").text(product["price"]);
+			var foodname=$("<div class='foodn'></div>").text(product["item_name"]);
+			var sub="<input id='subt1' name='subtract' value='-' type='button'>";
+			var box="<input id='qty' name='qty' value='0' style='width: 50px; text-align: center;' type='text'>";
+			var add="<input id='add1' name='add' value='+' type='button'>"
+			var veg=$("<span class='vegetarianicon'>⊡</span>");
+			var nv="<img src='img/nv.jpg' class='nonv'>";
+			//var non=$("<div class='abcd'></div>").text(product["is_vegetarian"]);
+		    var non= JSON.parse(product["is_vegetarian"]);
+			if (non==true) {
+			foodname=$(foodname).append(veg);
+			}
+			else {
+			foodname=$(foodname).append(nv);
+			}
+			foodname=$(foodname).append(sub,box,add);
+			elemen=$(elemen).append(imag,price,foodname);
+			
 			$(".area").append(elemen);
 			
 			});
